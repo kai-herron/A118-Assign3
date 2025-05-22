399f0d6425a9b8e5950c408fa312c4372e0c2709
Kai R. Herron <kaiherron28@gmail.com>
2025-05-22 17:44:18 -0400

    download file added

--- data/download_hst.py
+++ data/download_hst.py
@@ -28,15 +28,14 @@ BANDS = ['f606w','f814w']
 
 if __name__ == '__main__':
     cmd = 'wget https://archive.stsci.edu/hlsps/fhufd/hlsp_fhufd_hst_acs_{d}_{b}_v2.0_drz.fits'
-
+    
     for name in UFD_NAMES:
         for band in BANDS:
             if name not in SINGLEFIELD:
-                cmd.format(d=name+'01',b=band)
-                os.system(cmd)
+                os.system(cmd.format(d=name+'01',b=band))
             else:
-                cmd.format(d=name,b=band)
-                os.system(cmd)
+                
+                os.system(cmd.format(d=name,b=band))
 
     current_dir = os.getcwd()
     files = glob.glob(current_dir+'/*.fits')
