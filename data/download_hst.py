8af6902b4e6d1377be26b8ccc66df5bb08d878ee
Kai R. Herron <kaiherron28@gmail.com>
2025-05-31 19:11:14 -0400

    Isochrones working!

--- data/download_hst.py
+++ data/download_hst.py
@@ -28,14 +28,15 @@ BANDS = ['f606w','f814w']
 
 if __name__ == '__main__':
     cmd = 'wget https://archive.stsci.edu/hlsps/fhufd/hlsp_fhufd_hst_acs_{d}_{b}_v2.0_drz.fits'
-    
+    cmd_cat = 'wget https://archive.stsci.edu/hlsps/fhufd/hlsp_fhufd_hst_acs_{d}_multi_v2.0_cat.fits'
     for name in UFD_NAMES:
         for band in BANDS:
             if name not in SINGLEFIELD:
-                os.system(cmd.format(d=name+'01',b=band))
+                #os.system(cmd.format(d=name+'01',b=band))
+                os.system(cmd_cat.format(d=name))
             else:
-                
-                os.system(cmd.format(d=name,b=band))
+                #os.system(cmd.format(d=name,b=band))
+                os.system(cmd_cat.format(d=name))
 
     current_dir = os.getcwd()
     files = glob.glob(current_dir+'/*.fits')
